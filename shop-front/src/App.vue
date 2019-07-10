2d42455dfd48a67a596f110468c8e7c6ad517b36
inlove8307-shop <inlove8307.shop@gmail.com>
2019-07-10 08:51:20 +0000

    set main layout template

--- shop-front/src/App.vue
+++ shop-front/src/App.vue
@@ -1,29 +1,429 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+  </div> -->
+  <div id="app">
+    <header>
+      <h1>TITLE</h1>
+      <menu>
+        <a href="">MENU</a>
+        <a href="">MENU</a>
+        <a href="">MENU</a>
+      </menu>
+    </header>
+    <main>
+      <section>
+        <!-- <nav>
+          <a href="">LINK</a>
+          <a href="">LINK</a>
+          <a href="">LINK</a>
+        </nav> -->
+        <h2>
+          <p>SECTION TITLE</p>
+        </h2>
+        <article>
+          <h3>CATEGORY</h3>
+          <ul>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food01.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food02.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food03.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food04.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h3>CATEGORY</h3>
+          <ul>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food01.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food02.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food03.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food04.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h3>CATEGORY</h3>
+          <ul>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food01.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food02.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food03.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food04.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h3>CATEGORY</h3>
+          <ul>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food01.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food02.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food03.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food04.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h3>CATEGORY</h3>
+          <ul>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food01.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food02.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food03.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+            <li>
+              <span class="food">
+                <span><img src="./assets/images/img_food04.jpg" alt="food"></span>
+                <strong>NAME</strong>
+                <p>SUMMURY</p>
+                <a href="">DETAIL</a>
+              </span>
+            </li>
+          </ul>
+        </article>
+      </section>
+    </main>
+    <!-- <aside>
+      <nav>
+        <a href="">LINK</a>
+        <a href="">LINK</a>
+        <a href="">LINK</a>
+      </nav>
+    </aside> -->
+    <footer>copyright</footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  height: 100%;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+html {
+  font-size: 0.625rem;
+}
+
+body {
+  margin: 0;
+  background-image: url('./assets/images/bg_main.jpg');
+  background-repeat: repeat;
+  background-position: 0 0;
+  background-size: 100%;
+  background-attachment: fixed;
+}
+
+#app {
+  min-height: 100%;
+  background: -moz-linear-gradient(top, rgba(0, 0, 0, 0.85) 0%, rgba(0, 0, 0, 0.3) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0.85) 0%,rgba(0, 0, 0, 0.3) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.85) 0%,rgba(0, 0, 0, 0.3) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 10rem;
+  border-bottom: 0.1rem solid rgba(255, 255, 255, 0.5);
+  text-align: center;
+
+  h1 {
+    margin: 0;
+    font-weight: 400;
+    font-size: 1.6rem;
+    color: rgba(255, 255, 255, 1);
+  }
+
+  menu {
+    position: absolute;
+    top: 50%;
+    right: 1rem;
+    margin: -1rem 0 0 0;
+    padding: 0;
+
+    a {
+      margin-left: 0.5rem;
+      font-size: 1.4rem;
+      color: rgba(255, 255, 255, 1);
     }
   }
 }
+
+main {
+  nav {
+    a {}
+  }
+  section {
+    padding: 1rem 0 0 0;
+
+    h2 {
+      position: relative;
+      margin: 0 1rem 0 1rem;
+      height: 30rem;
+      border-bottom: 0.2rem solid rgba(255, 255, 255, 0.7);
+      background-image: url('./assets/images/bg_section.jpg');
+      background-repeat: repeat;
+      background-position: 0 50%;
+      background-size: 100%;
+
+      p {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        background: -moz-linear-gradient(top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%); /* FF3.6-15 */
+        background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0) 0%,rgba(0, 0, 0, 1) 100%); /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%,rgba(0, 0, 0, 1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        text-align: center;
+        font-weight: 400;
+        font-size: 1.6rem;
+        color: rgba(255, 255, 255, 1);
+      }
+    }
+    article {
+      padding: 1rem;
+
+      h3 {
+        margin: 0;
+        padding: 1rem 0;
+        border-bottom: 0.1rem solid rgba(255, 255, 255, 0.5);
+        font-weight: 400;
+        font-size: 1.6rem;
+        color: rgba(255, 255, 255, 1);
+      }
+
+      ul {
+        margin: 1rem -1rem 0 0;
+        padding: 0;
+
+        &::after {
+          content: '';
+          display: block;
+          clear: both;
+        }
+
+        li {
+          float: left;
+          width: 25%;
+          list-style: none;
+
+          .food {
+            overflow: hidden;
+            display: block;
+            margin: 0 1rem 1rem 0;
+            border-radius: 0.5rem;
+            background-color: rgba(255, 255, 255, 0.2);
+            box-shadow: 0.1rem 0.1rem 0.5rem rgba(0, 0, 0, 0.5);
+
+            span {
+              overflow: hidden;
+              display: block;
+              margin: 1rem 1rem 0 1rem;
+              height: 30rem;
+              border-top: 0.1rem solid rgba(255, 255, 255, 0.7);
+              font-size: 1.4rem;
+              color: rgba(255, 255, 255, 1);
+
+              img {
+                width: 100%;
+              }
+            }
+
+            strong {
+              display: block;
+              margin: 1rem 1rem 0 1rem;
+              font-weight: 400;
+              font-size: 1.4rem;
+              color: rgba(255, 255, 255, 1);
+            }
+
+            p {
+              margin: 1rem 1rem 0 1rem;
+              font-size: 1.4rem;
+              color: rgba(255, 255, 255, 1);
+            }
+
+            a {
+              display: block;
+              margin: 1rem;
+              text-align: center;
+              font-size: 1.4rem;
+              color: rgba(255, 255, 255, 1);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+aside {
+  nav {
+    a {}
+  }
+}
+
+footer {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  height: 10rem;
+  background-color: rgba(0, 0, 0, 0.85);
+  font-size: 1.6rem;
+  color: rgba(255, 255, 255, 1);
+}
+
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
