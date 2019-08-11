5d2b1352cf551883c2c958bcd4a8758709c04356
KimTaehoon <inlove8307@gmail.com>
2019-08-11 09:45:21 -0700

    firebase config

--- shop-front/src/main.js
+++ shop-front/src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyZLtGxng4yE5x5m8oI7-NSpXpEdjhX2Q",
+  authDomain: "shop-bfa65.firebaseapp.com",
+  databaseURL: "https://shop-bfa65.firebaseio.com",
+  projectId: "shop-bfa65",
+  storageBucket: "shop-bfa65.appspot.com",
+  messagingSenderId: "688211696508",
+  appId: "1:688211696508:web:3cebcd65499673a2"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
